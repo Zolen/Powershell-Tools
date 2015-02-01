7f0523d24a3492e11cfc61ecd96b1786e40bf4c1
Zolen <kzolen@gmail.com>
2015-01-31 23:09:36 -0700

    Invoke-Voice Function

---
+++ Invoke-Voice.ps1
@@ -0,0 +1,10 @@
+﻿$voice = New-Object -ComObject SAPI.SPVoice
+$voice.Rate = -3
+ 
+function invoke-speech{
+    param([Parameter(ValueFromPipeline=$true)][string] $say )
+    process{
+        $voice.Speak($say) | out-null;    
+        }
+    }
+new-alias -name out-voice -value invoke-speech;
